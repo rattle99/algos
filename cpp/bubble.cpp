a9b93decc2193da8a6307130bf97c0aacb3824a5
rattle99 <24495512+rattle99@users.noreply.github.com>
2018-08-24 20:44:08 +0530

    commit

--- cpp/bubble.cpp
+++ cpp/bubble.cpp
@@ -1,7 +1,47 @@
+/*Program for bubble sort implementation 
+  in C++. */
+
 #include <iostream>
+#include <vector>
+
+std::vector<int> bubble_sort(std::vector<int> A)
+{	
+	std::cout<<"Current list : \n";
+	for(int i=0;i<A.size();i++)
+	{
+		std::cout<<A[i]<<" ";
+	}
+	std::cout<<"\nSorted list : \n";
+
+	for(int i=0;i<A.size()-1;i++)
+	{
+		for(int j=0;j<A.size()-i-1;j++)
+		{
+			if(A[i]>A[i+1])
+			{
+				int temp=A[i+1];
+				A[i+1]=A[i];
+				A[i]=temp;
+			}
+		}
+	}
+
+	for(int i=0;i<A.size();i++)
+        {
+                std::cout<<A[i]<<" ";
+        }
+	return A;
+}
 
 int main()
 {
-	std::cout<<"Test";
+	std::cout<<"\nTest\n";
+	std::vector<int> arr={10,2,23,-4,235,56,2,6,5,5,5,23,-4,346,-56,-54};
+	bubble_sort(arr);
+	std::cout<<"\n\n\n";
+	for(int i=0;i<arr.size();i++)
+        {
+                std::cout<<arr[i]<<" ";
+        }
 	return 0;
 }
