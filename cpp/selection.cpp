a44861a8cf3de62c4ceb1b1fec3d7d4a0c4eb8f5
rattle99 <24495512+rattle99@users.noreply.github.com>
2018-08-24 23:11:16 +0530

    add selection sort

---
+++ cpp/selection.cpp
@@ -0,0 +1,47 @@
+/*Program for selection sort implementation in C++. */
+
+#include <iostream>
+#include <vector>
+
+std::vector<int> selection_sort(std::vector<int> A)
+{
+        std::cout<<"Current list : \n";
+        for(int i=0;i<A.size();i++)
+        {
+                std::cout<<A[i]<<" ";
+        }
+        std::cout<<"\nSorted list : \n";
+
+        for(int i=0;i<A.size()-2;i++)
+        {
+		int imin=i;
+		for(int j=i+1;j<A.size()-1;j++)
+		{
+			if(A[j]<A[imin])
+				imin=j;
+		}
+		int temp=A[i];
+		A[i]=A[imin];
+		A[imin]=temp;
+        }
+
+        for(int i=0;i<A.size();i++)
+        {
+                std::cout<<A[i]<<" ";
+        }
+        return A;
+}
+
+int main()
+{
+        std::cout<<"\nTest\n";
+        std::vector<int> arr={10,2,23,-4,235,56,2,6,5,5,5,23,-4,346,-56,-54};
+        selection_sort(arr);
+        std::cout<<"\n\n\n";
+        for(int i=0;i<arr.size();i++)
+        {
+                std::cout<<arr[i]<<" ";
+        }
+        return 0;
+}
+
