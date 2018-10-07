d4d350b06f752648868d1450429d8f397b980648
rattle99 <24495512+rattle99@users.noreply.github.com>
2018-10-07 11:06:36 +0530

    add linked list as stack

---
+++ linked-list/stack.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+struct node {
+	int data;
+	node *next;
+};
+
+class stack {
+	node *top;
+	public:
+	stack() {
+		top=NULL;
+	}
+	void push(int);
+	void pop();
+	void show();
+};
+
+void stack::push(int a) {
+	node *temp;
+	temp=new node;
+	temp->data=a;
+	temp->next=NULL;
+	if(top!=NULL) {
+		temp->next=top;
+	}
+	top=temp;
+}
+
+void stack::pop() {
+	if(top!=NULL) {
+		top=top->next;
+	}
+}
+
+void stack::show() {
+	node *temp=top;
+	while (temp!=NULL) {
+		std::cout << temp->data << " ";
+		temp=temp->next;
+	}
+}
+
+int main() {
+	stack s;
+	for (int i = 0; i < 10; i++) {
+		s.push(i);
+	}
+	std::cout<<"\n\n";
+	s.show();
+	std::cout<<"\n\n";
+	for (int i = 0; i < 10; i+=2) {
+		s.pop();
+	}
+	s.show();
+	return 0;
+}
