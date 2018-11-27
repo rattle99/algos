4e49406e106b69464f1f4e74ec46d737c71909b0
rattle99 <24495512+rattle99@users.noreply.github.com>
2018-11-27 20:30:57 +0530

    include <algoorithms>

--- sorting/tool.h
+++ sorting/tool.h
@@ -1,8 +1,13 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 void print(std::vector<int> &A) {
       for(int i:A) std::cout<<i<<" ";
+}
+
+void print(std::vector<float> &A) {
+      for(float i:A) std::cout<<i<<" ";
 }
 
 void swap(int &x, int &y) {
