0ddd3f0bc3b3b51cbcb35042f1742e3a9db7a717
rattle99 <24495512+rattle99@users.noreply.github.com>
2018-11-28 17:34:08 +0530

    add singly linked list

--- linked-list/singly-linked.cpp
+++ linked-list/singly-linked.cpp
@@ -7,12 +7,71 @@ struct node {
 
 node *head;
 
-void insert(int n)
+void insertBeg(int x)
 {
 	node *temp = new node;
-	temp->data = n;
+	temp->data = x;
 	temp->next = head;
 	head = temp;
+}
+
+void insert(int x, int n)
+{
+	node *temp = new node;
+	temp->data = x;
+	temp->next = NULL;
+	if (n==1) {
+		temp->next = head;
+		head = temp;
+		return;
+	}
+	node *temp2 = head;
+	for (int i = 0; i < n-2; i++) {
+		temp2 = temp2->next;
+	}
+	temp->next = temp2->next;
+	temp2->next = temp;
+}
+
+void insertEnd(int x)
+{
+	node *temp = new node;
+	temp->data = x;
+	temp->next = NULL;
+
+	node *temp2 = head;
+	while (temp2->next != NULL) {
+		temp2 = temp2->next;
+	}
+	temp2->next = temp;
+}
+
+void deleteNode(int n)
+{
+	node *temp = head;
+	if (n==1) {
+		head = temp->next;
+		delete temp;
+		return;
+	}
+	for (int i = 0; i < n-2; i++) {
+		temp =  temp->next;
+	}
+	node *temp2 = temp->next;
+	temp->next = temp2->next;
+	delete temp2;
+}
+
+void deleteEnd()
+{
+	node *temp = head;
+	node *temp2 = new node;
+	while (temp->next != NULL) {
+		temp2 = temp;
+		temp = temp->next;
+	}
+	delete temp;
+	temp2->next = NULL;
 }
 
 void display()
@@ -33,7 +92,25 @@ int main()
 	int i=8;
 	while(i--)
 	{
-		insert(i);
+		insertBeg(i);
+		insert(i+1,1);
+		insert(i+10,3);
+		display();
+	}
+	i=15;
+	std::cout << "----------" << '\n';
+	while (i--) {
+		deleteNode(3);
+		display();
+	}
+	i=5;
+	while (i--) {
+		insertEnd(i);
+		display();
+	}
+	i=5;
+	while (i--) {
+		deleteEnd();
 		display();
 	}
 	return 0;
